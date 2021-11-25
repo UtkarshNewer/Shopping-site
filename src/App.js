e2b59fefce4741b4e8298973431b38e67398b2a4
Dante26us <utkarshshishodiauk@gmail.com>
2021-11-25 20:49:13 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Help from "./pages/Help";
+import Home from "./pages/Home";
+import PaymentOptions from "./pages/PaymentOptions";
+import PlaceOrder from "./pages/PlaceOrder";
+import ProductDescription from "./pages/ProductDescription";
+import ShippingDetails from "./pages/ShippingDetails";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/shop">
+          <Shop />
+        </Route>
+        <Route exact path="/help">
+          <Help />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/shop/:id">
+          <ProductDescription />
+        </Route>
+        <Route exact path="/order-details">
+          <ShippingDetails />
+        </Route>
+        <Route exact path="/payment">
+          <PaymentOptions />
+        </Route>
+        <Route exact path="/place-order">
+          <PlaceOrder />
+        </Route>
+        <Route path="/*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
